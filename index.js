20b049655e9b0ec01048f26499b3cce5c4086cee
Kyan <84374752+kyan0045@users.noreply.github.com>
2024-04-05 22:47:39 +0200

    v1.1.0 - Patch of unnecessary error

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-version = "1.1.0";
+let version = "1.1.0";
 // Version counter
 
 // Importing required packages
@@ -19,7 +19,7 @@ if (config?.logWebhook?.length > 25) {
 let data = process.env.TOKENS || fs.readFileSync("./tokens.txt", "utf-8");
 if (!data) throw new Error(`Unable to find your tokens.`);
 const tokensAndChannelIds = data.split(/\s+/);
-config.tokens = [];
+let config.tokens = [];
 for (let i = 0; i < tokensAndChannelIds.length; i += 2) {
   if (tokensAndChannelIds[i + 1]) {
     const token = tokensAndChannelIds[i].trim();
@@ -111,7 +111,7 @@ async function start() {
         icon_url: "https://avatars.githubusercontent.com/u/84374752?v=4",
       },
     };
-  log.send({
+  log?.send({
     username: "Spammer Logs",
     avatarURL: "https://avatars.githubusercontent.com/u/84374752?v=4",
     embeds: [embed],
