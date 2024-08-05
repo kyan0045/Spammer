099fc8a26a11091abb569b8d85b2c9a5db58647f
Xenon <interstel.larproxy@gmail.com>
2024-08-05 00:02:36 +0000

    v1.2.0 - Adding option to auto-delete messages, prevent leaking private tokens & webhooks

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
-let version = "1.1.0";
-// Version counter
+version = "1.1.0";
 
-// Importing required packages
 const { Client, WebhookClient } = require("discord.js-selfbot-v13");
 const fs = require("fs-extra");
 const chalk = require("chalk");
-
-// Deciding which config to use
 const config = process.env.CONFIG
   ? JSON.parse(process.env.CONFIG)
   : require("./config.json");
@@ -15,11 +11,11 @@ if (config?.logWebhook?.length > 25) {
   log = new WebhookClient({ url: config.logWebhook });
 }
 
-// Getting & seperating the tokens
 let data = process.env.TOKENS || fs.readFileSync("./tokens.txt", "utf-8");
 if (!data) throw new Error(`Unable to find your tokens.`);
 const tokensAndChannelIds = data.split(/\s+/);
 config.tokens = [];
+
 for (let i = 0; i < tokensAndChannelIds.length; i += 2) {
   if (tokensAndChannelIds[i + 1]) {
     const token = tokensAndChannelIds[i].trim();
@@ -31,22 +27,19 @@ for (let i = 0; i < tokensAndChannelIds.length; i += 2) {
   }
 }
 
-// Replit .env check
 if (process.env.REPLIT_DB_URL && (!process.env.TOKENS || !process.env.CONFIG))
   console.log(
-    `You are running on replit, please use it's secret feature, to prevent your tokens and webhook from being stolen and misused.\nCreate a secret variable called "CONFIG" for your config, and a secret variable called "TOKENS" for your tokens.`
+    `You are running on replit, please use its secret feature, to prevent your tokens and webhook from being stolen and misused.\nCreate a secret variable called "CONFIG" for your config, and a secret variable called "TOKENS" for your tokens.`
   );
 
-// Main function which handles the actual spamming
 async function Login(token, channelId) {
- 
-  // Checks to see if the values are valid
   if (!token) {
     console.log(
       chalk.redBright("You must specify a (valid) token.") +
         chalk.white(` ${token} is invalid.`)
     );
   }
+
   if (!channelId) {
     console.log(
       chalk.redBright(
@@ -54,6 +47,7 @@ async function Login(token, channelId) {
       )
     );
   }
+
   if (channelId && channelId.length > 21) {
     console.log(
       chalk.redBright(
@@ -62,8 +56,8 @@ async function Login(token, channelId) {
     );
   }
 
-  // Initiating the djs-selfbot client and logging in
   const client = new Client({ checkUpdate: false, readyStatus: false });
+
   client.login(token).catch(() => {
     console.log(
       `Failed to login with token "${chalk.red(
@@ -72,7 +66,6 @@ async function Login(token, channelId) {
     );
   });
 
-  // Ready event which starts the spammer
   client.on("ready", async () => {
     console.log(`Logged in to ` + chalk.red(client.user.tag) + `!`);
     client.user.setStatus("invisible");
@@ -87,14 +80,18 @@ async function Login(token, channelId) {
       .readFileSync("./data/messages.txt", "utf-8")
       .split("\n");
 
-    setInterval(() => {
+    setInterval(async () => {
       const message = messages[Math.floor(Math.random() * messages.length)];
-      spamChannel.send(message);
+      const sentMessage = await spamChannel.send(message);
+      
+      // Check if delete is set to true in config
+      if (config.delete) {
+        sentMessage.delete().catch(err => console.log(chalk.red("Failed to delete message:", err)));
+      }
     }, config.spamSpeed);
   });
 }
 
-// Function that runs the main function with every available token
 async function start() {
   for (var i = 0; i < config.tokens.length; i++) {
     await Login(config.tokens[i].token, config.tokens[i].channelId);
@@ -102,41 +99,43 @@ async function start() {
   if (log)
     embed = {
       title: `Started!`,
-      url: "https://github.com/kyan0045/Spammer",
+      url: "https://github.com/XenDevs/Spammer",
       description: `Found ${config.tokens.length} tokens!`,
       color: "#5cf7a9",
       timestamp: new Date(),
       footer: {
-        text: "Spammer by @kyan0045",
+        text: "Spammer by @XenDevs",
         icon_url: "https://avatars.githubusercontent.com/u/84374752?v=4",
       },
     };
-  log?.send({
+  log.send({
     username: "Spammer Logs",
     avatarURL: "https://avatars.githubusercontent.com/u/84374752?v=4",
     embeds: [embed],
   });
 }
 
-// Error handling
 process.on("unhandledRejection", (reason, p) => {
   if (config.debug) {
     console.log(" [Anti Crash] >>  Unhandled Rejection/Catch");
     console.log(reason, p);
   }
 });
+
 process.on("uncaughtException", (e, o) => {
   if (config.debug) {
     console.log(" [Anti Crash] >>  Uncaught Exception/Catch");
     console.log(e, o);
   }
 });
+
 process.on("uncaughtExceptionMonitor", (err, origin) => {
   if (config.debug) {
     console.log(" [AntiCrash] >>  Uncaught Exception/Catch (MONITOR)");
     console.log(err, origin);
   }
 });
+
 process.on("multipleResolves", (type, promise, reason) => {
   if (config.debug) {
     console.log(" [AntiCrash] >>  Multiple Resolves");
@@ -144,5 +143,4 @@ process.on("multipleResolves", (type, promise, reason) => {
   }
 });
 
-// Starting the program
 start();
